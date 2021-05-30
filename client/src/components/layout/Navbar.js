a7995ec1e7006973819330245c512ca775013300
sauravrajleaf <saurved785@gmail.com>
2021-05-30 17:54:42 +0530

    Created auth component folder(Login and Register components)

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () =>{
+const Navbar = () => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>Contact Manager</h1>
+      <ul>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div className="navbar bg-primary">
-            <h1>Contact Manager</h1>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
-
-export default Navbar ; 
+export default Navbar;
