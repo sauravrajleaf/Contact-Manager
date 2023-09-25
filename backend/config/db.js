c4089bf080dfeb4bf76a1bdc1c903427c4b321c8
saurav raj <saurved785@gmail.com>
2023-09-25 23:36:33 +0530

    modified db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,12 @@ console.log(process.env.mongoURI);
 //MONGOOSE RETURNS PROMISES
 const connectDB = async () => {
 	try {
-		await mongoose.connect(process.env.mongoURI, {});
+		await mongoose.connect(process.env.mongoURI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
 		console.log('MongoDB Connected');
 	} catch (err) {
 		console.log(err.message);
